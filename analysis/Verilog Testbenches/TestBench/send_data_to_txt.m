cb937e812ba6e560bde68d845d086a74cecfeded
SB <stefano.buccelli@gmail.com>
2019-07-08 12:37:07 +0200

    the current version of ISE testbench works with R18-159_2019_01_31_1 (we need to upload the rhs)

--- analysis/Verilog Testbenches/TestBench/send_data_to_txt.m
+++ analysis/Verilog Testbenches/TestBench/send_data_to_txt.m
@@ -1,9 +1,6 @@
 %% send data to txt
 clear
-% load('C:\Users\BuccelliLab\Documents\GitHub\intan_project\debugging\sample_data\data_reduced_r17.mat')
-% read_Intan_RHS2000_file('C:\Users\BuccelliLab\Desktop\Prova_intan\Ordered_recordings\HPF_250Hz_200uV_A004_190108_151822.rhs')
-% read_Intan_RHS2000_file('C:\Users\BuccelliLab\Desktop\Prova_intan\Ordered_recordings\HPF_3000Hz_50uV_A004_190114_103812.rhs')
-read_Intan_RHS2000_file('C:\Users\BuccelliLab\Documents\GitHub\intan-dac-debug\R18-00_2018_12_18\R18-00_2018_12_18_1.rhs')
+read_Intan_RHS2000_file % if you don't want to change parameters, you have to import R18-159_2019_01_31_1_190131_152307
 %% don't forget to modify the filter in Verilog with this number:
 fs=30e3;
 fc=300;
@@ -11,13 +8,17 @@ b = 1.0 - exp(-2.0 * 3.1415926535897 * fc / fs);
 filterCoefficient = floor(65536.0 * b + 0.5);
 
 %% thresholds
-th_1=-100;
-th_2=-350;
-th_1_to_tb=floor(32768+th_1/0.195);
-th_2_to_tb=floor(32768+th_2/0.195);
+th_1=round(-46/0.195)*0.195; %uV
+th_1_to_tb=round(th_1/0.195)+ 32768; %uint16 This is as in Qt the threshold is sent to the FPGA
+th_2=round(-66/0.195)*0.195; %uV
+th_2_to_tb=round(th_2/0.195)+ 32768; %uint16 This is as in Qt the threshold is sent to the FPGA
+th_3=round(-13/0.195)*0.195; %uV
+th_3_to_tb=round(th_3/0.195)+ 32768; %uint16 This is as in Qt the threshold is sent to the FPGA
+th_4=round(39/0.195)*0.195; %uV
+th_4_to_tb=round(th_4/0.195)+ 32768; %uint16 This is as in Qt the threshold is sent to the FPGA
 
 %% plot to look at the data
-ch_indx_ampl=1;
+ch_indx_ampl=3;
 % ch_indx_ampl=1;
 t=(1:1:size(amplifier_data,2))./fs;
 amplif=amplifier_data(ch_indx_ampl,:);
@@ -25,13 +26,15 @@ dac=board_dac_data(1,:);
 figure
 h(1)=subplot(2,1,1);
 plot(t,amplif)
+title('amplifier')
 h(2)=subplot(2,1,2);
 plot(t,dac)
+title('dac')
 linkaxes(h,'x')
 %% start and stop sample to send binary
 
 start_sample=1;
-stop_sample=60e3;
+stop_sample=size(amplifier_data,2);
 
 amplifier_u16=32768+amplifier_data(ch_indx_ampl,start_sample:stop_sample)/0.195;
  
